cf4f5a29ac0b6031ba7512048ea5ac0d8da2e080
Adam Hardy <ahardy55@gmail.com>
2013-11-06 13:58:32 -0500

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+MyRepNews::Application.config.secret_key_base = 'e3fc14d0765303dbe5029f11f9acb6d2509393cf5a5f2b2d88e2ffbd25f8102d8e17b3bf87304077457037c1486768d9dfc65993fcd6d0837db05642c5bca2bf'
