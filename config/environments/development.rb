ff51f04a56665aa85ca711133e86120f22650f4d
Adam Hardy <ahardy55@gmail.com>
2013-12-13 11:50:43 -0500

    listed reps in directory

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -26,4 +26,5 @@ MyRepNews::Application.configure do
   # This option may cause significant delays in view rendering with a large
   # number of complex assets.
   config.assets.debug = true
+  config.action_mailer.default_url_option = { :host => 'localhost:3000' }
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,10 @@
 MyRepNews::Application.routes.draw do
+  #devise_for :users
   get "static_pages/home"
   resources :represenatives
 
   get "represenative/index"
+  get "represenative/show"
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
